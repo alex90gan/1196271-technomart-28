bf356c3927ea713a6908bc0d4464b3efd9001e40
alex90gan <65192549+alex90gan@users.noreply.github.com>
2020-06-21 15:37:18 +0300

    map-modal

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ let writeUsButton = document.querySelector('.write-us-button');
 let writeUs = document.querySelector('.write-us');
 let closeWriteUs = document.querySelector('.close-btn-write-us');
 
-
+let mapButtonActive = document.querySelector('map-button-active')
+let modalMap = document.querySelector('modal-map')
+let closeBtnMap = document.querySelector('.close-btn-map')
 
 cardButtonsBuy.forEach(function(item) {
     item.addEventListener('click', function() {
@@ -27,4 +29,12 @@ writeUsButton.addEventListener('click', function() {
 
 closeWriteUs.addEventListener('click', function() {
     writeUs.classList.remove('write-us-show');
+});
+
+mapButtonActive.addEventListener('click', function() {
+    modalMap.classList.add('modal-map-show');
+});
+
+closeBtnMap.addEventListener('click', function() {
+    modalMap.classList.remove('modal-map-show');
 });
